0f878e04592ce1c4366285a14d20dfdd4d17c865
Jonny Shuttleworth <jonny.shuttleworth@emishealth.com>
2020-03-06 14:05:06 +0000

    0.22

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit } = require("@octokit/action");
+const octokit = new Octokit();
 
-async function getColumns(projectId) {
-  return await octokit.request("GET /projects/:project_id/columns", {
-    project_id: projectId,
+const getProjects = async function(repoInfo, projectMatchRegex) {
+  const projectList = await octokit.request("GET /repos/:owner/:repo/projects", {
+    owner: repoInfo.owner,
+    repo: repoInfo.repo,
     headers: {
       'accept': 'application/vnd.github.inertia-preview+json'
     }
   });
+
+  // Filter these down by project names that match the Regex we were given
+  return projectList.data.filter(project => projectMatchRegex.test(project.name)).map(project => project.id);
 }
 
-async function getCardIds(columnIds) {
-  return await Promise.all(columnIds.map(
-    columnId => {
-      octokit.request("GET /projects/columns/:column_id/cards", {
+const getCardIdsFromProjects = async function(repoProjects) {
+  let cards = []
+
+  // Get all the project columns
+  for (let i = 0; i < repoProjects.length; i++) {
+    let res = await octokit.request("GET /projects/:project_id/columns", {
+      project_id: repoProjects[i],
+      headers: {
+        'accept': 'application/vnd.github.inertia-preview+json'
+      }
+    });
+
+    // Got all the columns for this project, now we need cards
+    const columnIds = res.data.map(project => project.id)
+
+    res = await Promise.all(columnIds.map(
+      columnId => octokit.request("GET /projects/columns/:column_id/cards", {
         column_id: columnId,
         headers: {
           'accept': 'application/vnd.github.inertia-preview+json'
         }
-      });
-    }
-  ));
+      })
+    ));
+
+    // We have all the cards from all the columns, put them together
+    res.forEach(card => {
+      cards = cards.concat(card.data)
+    })
+
+    return cards;
+  }
+
+  return cards.map(card => card.content_url)
 }
 
 async function run() {
-  // Get Octokit running
-  const octokit = new Octokit();
-
   try {
     // Get the Regex from the YAML
     const regexString = core.getInput('match-regex');
@@ -39,48 +63,26 @@ async function run() {
       return;
     }
 
-    // Go find projects for this repo
-    const repoInfo = github.context.repo;
-    const projectList = await octokit.request("GET /repos/:owner/:repo/projects", {
-      owner: repoInfo.owner,
-      repo: repoInfo.repo,
-      headers: {
-        'accept': 'application/vnd.github.inertia-preview+json'
-      }
-    });
-
-    // Filter them down by project names that match the Regex we were given
-    let repoProjects = projectList.data.filter(project => projectMatchRegex.test(project.name)).map(project => project.id);
+    // Go find projects for this repo that match our Regex
+    const repoProjects = await getProjects(github.context.repo, projectMatchRegex);
 
     // Then get the cards for all those valid projects
-    let projectCards = [];
-    repoProjects.forEach(async (projectId) => {
-      let columnsFromId = await getColumns(projectId);
-
-      // Got all the columns for this project, now we need cards
-      const columnIds = columnsFromId.data.map(project => project.id);
-      let cardsFromColumnId = await getCardIds(columnIds);
-
-      // We have all the cards from all the columns, put them together
-      cardsFromColumnId.forEach(card => {
-        projectCards = projectCards.concat(card.data)
-      })
-    });
+    const projectCards = await getCardIdsFromProjects(repoProjects);
 
     const payload = github.context.payload;
 
     // Pull the issue IDs out of the cards
-    let issueUrl = payload.repository.issues_url;
-    issueUrl = issueUrl.substring(0, issueUrl.length - ('{/number}').length);
-
     let cardIssues = [];
-    projectCards.forEach((card) => {
+    let issueUrl = payload.repository.issues_url;
+    issueUrl = issueUrl.substring(0, issueUrl.length - ('{/number}').length)
+
+    for (let card of projectCards) {
       const match = card.indexOf(issueUrl)
 
       if (match !== -1) {
         cardIssues.push(card.substring(match + issueUrl.length + 1))
       }
-    });
+    }
 
     // Get the current PR number from the payload
     const thisIssueNumber = payload.number;
