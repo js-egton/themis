2a9dae2bb8e3642f60179ac321b816d1745e92de
Jonny Shuttleworth <jonny.shuttleworth@emishealth.com>
2020-03-12 12:00:04 +0000

    v2.6 - adds label validator

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const github = require('@actions/github');
 const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 
+const getLabelsOnIssue = async function(repoInfo, issueNumber) {
+  const labelsList = await octokit.request("GET /repos/:owner/:repo/issues/:issue_number/labels", {
+    owner: repoInfo.owner,
+    repo: repoInfo.repo,
+    issue_number: issueNumber
+  });
+
+  return labelsList;
+}
+
 const getProjects = async function(repoInfo, projectMatchRegex) {
   const projectList = await octokit.request("GET /repos/:owner/:repo/projects", {
     owner: repoInfo.owner,
@@ -67,47 +77,80 @@ const getIssuesFromCards = async function(payload, projectCards) {
   return issues;
 }
 
+const checkProjectRegex = async function(regex) {
+  const projectMatchRegex = new RegExp(regex);
+
+  // If it's not valid Regex, get outta here
+  if (! projectMatchRegex) {
+    core.setFailed(regexString + ' is not valid Regex, exiting.');
+  }
+
+  // Go find projects for this repo that match our Regex
+  const repoProjects = await getProjects(github.context.repo, projectMatchRegex);
+
+  if (repoProjects.length < 1) {
+    core.setFailed('No projects found that matched given Regex: ' + projectMatchRegex);
+    return;
+  }
+
+  // Then get the cards for all those valid projects
+  const projectCards = await getCardIdsFromProjects(repoProjects);
+
+  if (projectCards.length < 1) {
+    core.setFailed('No cards found for matching Projects: ' + (repoProjects || null));
+    return;
+  }
+
+  // Pull the issue IDs out of the cards
+  const cardIssues = await getIssuesFromCards(github.context.payload, projectCards);
+
+  if (cardIssues.length < 1) {
+    core.setFailed('No issues found in Project Cards: ' + (projectCards || null));
+    return;
+  }
+
+  // Get the current PR number from the payload
+  const thisIssueNumber = github.context.payload.number;
+
+  // If the PR issue is included in the big list of card IDs, we're good
+  if (! cardIssues.includes(thisIssueNumber)) {
+    core.setFailed('PR must be in a valid sprint project to be merged.')
+  }
+}
+
+const checkLabelRegex = async function(regex) {
+  const labelMatchRegex = new RegExp(regex);
+
+  // If it's not valid Regex, get outta here
+  if (! labelMatchRegex) {
+    core.setFailed(regexString + ' is not valid Regex, exiting.');
+  }
+
+  const prLabels = await getLabelsOnIssue(github.context.repo, github.context.payload.number);
+
+  // If there's no labels, let this check go clean
+  if (prLabels.length > 0) {
+    prLabels.forEach((label) => {
+      if (labelMatchRegex.test(label.name)) {
+        // Label matches given Regex, quit out
+        core.setFailed('PR has ' + label.name + ' label attached, so it cannot be merged.')
+      }
+    });
+  }
+}
+
 async function run() {
   try {
     // Get the Regex from the YAML
-    const regexString = core.getInput('match-regex');
-    const projectMatchRegex = new RegExp(regexString);
+    const projectRegex = core.getInput('project-regex');
+    const labelRegex = core.getInput('label-regex');
 
-    // If it's not valid Regex, get outta here
-    if (! projectMatchRegex) {
-      core.setFailed(regexString + ' is not valid Regex, exiting.');
+    if (projectRegex) {
+      checkProjectRegex(projectRegex);
     }
 
-    // Go find projects for this repo that match our Regex
-    const repoProjects = await getProjects(github.context.repo, projectMatchRegex);
-
-    if (repoProjects.length < 1) {
-      core.setFailed('No projects found that matched given Regex: ' + projectMatchRegex);
-      return;
-    }
-
-    // Then get the cards for all those valid projects
-    const projectCards = await getCardIdsFromProjects(repoProjects);
-
-    if (projectCards.length < 1) {
-      core.setFailed('No cards found for matching Projects: ' + (repoProjects || null));
-      return;
-    }
-
-    // Pull the issue IDs out of the cards
-    const cardIssues = await getIssuesFromCards(github.context.payload, projectCards);
-
-    if (cardIssues.length < 1) {
-      core.setFailed('No issues found in Project Cards: ' + (projectCards || null));
-      return;
-    }
-
-    // Get the current PR number from the payload
-    const thisIssueNumber = github.context.payload.number;
-
-    // If the PR issue is included in the big list of card IDs, we're good
-    if (! cardIssues.includes(thisIssueNumber)) {
-      core.setFailed('PR must be in a valid sprint project to be merged.')
+    if (labelRegex) {
+      checkLabelRegex(labelRegex);
     }
   } catch (error) {
     core.setFailed(error.message);
